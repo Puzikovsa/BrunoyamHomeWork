22c99f30f5a0fcd3d7333aada838ac260389cb3c
Puzikovsa <80515484+Puzikovsa@users.noreply.github.com>
2021-10-17 19:44:32 +0300

    TicTack 17.10 v.2

--- src/TicTacGame_New/ComputerPlayer.java
+++ src/TicTacGame_New/ComputerPlayer.java
@@ -1,6 +1,8 @@
 package TicTacGame_New;
 
-public class ComputerPlayer extends Player{
+import java.util.Random;
+
+public class ComputerPlayer extends Player {
 
 
     public ComputerPlayer(Cell team, String name) {
@@ -9,6 +11,28 @@ public class ComputerPlayer extends Player{
 
     @Override
     void move() {
+        gameMove(Controller.player);
+    }
 
+    private void gameMove(Player player) {
+        player = Controller.player;
+        int size = Field.getSize();
+        while (true) {
+            Random ran = new Random(size - 1);
+            double randomNumber = Math.floor(ran.nextDouble());
+            int row = (int) randomNumber;
+            Random col = new Random(size - 1);
+            double randomNamber1 = Math.floor(col.nextDouble());
+            int column = (int) randomNamber1;
+            if (row >= 0 && row < size && column >= 0 && column < size) {
+                if (Checks.isEmpty(row, column, Field.getField())) {
+                    Field.fillingField(row, column, player);
+                    Field.showField();
+                    break;
+                }
+            }
+        }
+        System.out.println("Переход хода");
+        Controller.changePlayer();
     }
 }
